8af124b8395c582a5af08a1c1116a9275343582d
Daniel Yepez Garces <56177970+danielyepezgarces@users.noreply.github.com>
2023-02-20 20:50:21 +0100

    Create banner.js

---
+++ banner.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+	atOptions = {
+		'key' : '67bbff5cbd2d2e7c833450678210cb25',
+		'format' : 'iframe',
+		'height' : 60,
+		'width' : 468,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/67bbff5cbd2d2e7c833450678210cb25/invoke.js"></scr' + 'ipt>');
+</script>
