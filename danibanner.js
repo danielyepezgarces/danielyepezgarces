9c2edbc8f3f5e5a3d5547e48d9af96b63d49ec01
Daniel Yepez Garces <56177970+danielyepezgarces@users.noreply.github.com>
2024-11-01 10:58:07 -0500

    Delete danibanner.js

--- danibanner.js
+++
@@ -1,9 +0,0 @@
-
-	atOptions = {
-		'key' : '67bbff5cbd2d2e7c833450678210cb25',
-		'format' : 'iframe',
-		'height' : 60,
-		'width' : 468,
-		'params' : {}
-	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.effectivecreativeformat.com/67bbff5cbd2d2e7c833450678210cb25/invoke.js"></scr' + 'ipt>');
